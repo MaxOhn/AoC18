0f613d357c3d47f3e4b3ac5dc87b4f663334101c
MaxOhn <ohn.m@hotmail.de>
2018-12-03 00:18:57 +0100

    day 2

--- AdventOfCode18/Program.cs
+++ AdventOfCode18/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿
+
+
+using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -12,11 +16,11 @@ namespace AdventOfCode18
         {
             try
             {
-                using (StreamReader sr = new StreamReader("../../../D1.txt"))
+                using (StreamReader sr = new StreamReader("../../../D2.txt"))
                 {
                     Stopwatch sw = new Stopwatch();
                     sw.Start();
-                    var answer = Day1(sr.ReadToEnd());
+                    var answer = Day2(sr.ReadToEnd());
                     sw.Stop();
                     Console.WriteLine($"Solution: {answer} [{sw.Elapsed}]");
                 }
@@ -44,5 +48,46 @@ namespace AdventOfCode18
                 }
             }
         } // 4.656s
+
+        private static Tuple<int, string> Day2(string input)
+        {
+            var boxIDs = input.Split(Environment.NewLine).ToList();
+            int twice = 0,  thrice = 0;
+            var charOccurences = new Dictionary<char, int>();
+            foreach (string boxID in boxIDs)
+            {
+                charOccurences.Clear();
+                foreach (char c in boxID)
+                {
+                    if (charOccurences.ContainsKey(c))
+                        charOccurences[c]++;
+                    else
+                        charOccurences.Add(c, 1);
+                }
+                twice += charOccurences.Values.Contains(2) ? 1 : 0;
+                thrice += charOccurences.Values.Contains(3) ? 1 : 0;
+            }
+            int p1 = twice * thrice, a = boxIDs.Count();
+            var currLetters = new List<char>();
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = i+1; j < a; j++)
+                {
+                    currLetters.Clear();
+                    int differ = 0, l = boxIDs[i].Count();
+                    for (int k = 0; k < l; k++)
+                    {
+                        currLetters.Add(boxIDs[i][k]);
+                        if (!boxIDs[i][k].Equals(boxIDs[j][k]))
+                            differ++;
+                        if (differ > 1)
+                            break;
+                    }
+                    if (differ == 1)
+                        return Tuple.Create(p1, string.Join("", currLetters));
+                }
+            }
+            return null;
+        } // 0.0155
     }
 }
